14a3d6d1155c3dafde407353b0ed178672e7063b
George Michelon <geomichelon@gmail.com>
2026-01-30 16:37:02 -0300

    Initial Commit

---
+++ DecoupledApps-iOS/DecoupledApps_iOSApp.swift
@@ -0,0 +1,32 @@
+//
+//  DecoupledApps_iOSApp.swift
+//  DecoupledApps-iOS
+//
+//  Created by George Michelon on 30/01/26.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct DecoupledApps_iOSApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
